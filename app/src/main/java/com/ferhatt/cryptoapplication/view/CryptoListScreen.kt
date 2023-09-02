0f156f3d2622189de0b877312d5488430fb4a859
ferhatseker180 <ferhatseker180@gmail.com>
2023-09-02 18:57:49 +0300

    Project started, Main Activity and Navigation finished. App Module, Services, Models and Repository created. We created searching algorithm.

--- app/src/main/java/com/ferhatt/cryptoapplication/view/CryptoListScreen.kt
+++ app/src/main/java/com/ferhatt/cryptoapplication/view/CryptoListScreen.kt
@@ -1,11 +1,25 @@
 package com.ferhatt.cryptoapplication.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.ferhatt.cryptoapplication.viewmodel.CryptoListViewModel
 
 @Composable
-fun CryptoListScreen(navController: NavController){
+fun CryptoListScreen(
+    navController: NavController,
+    viewModel : CryptoListViewModel = hiltViewModel()
+) {
 
-    Text(text = "Hello World")
+
+
+
 }
--- app/src/main/java/com/ferhatt/cryptoapplication/viewmodel/CryptoDetailViewModel.kt
+++ app/src/main/java/com/ferhatt/cryptoapplication/viewmodel/CryptoDetailViewModel.kt
@@ -1,4 +1,19 @@
 package com.ferhatt.cryptoapplication.viewmodel
 
-class CryptoDetailViewModel {
+import androidx.lifecycle.ViewModel
+import com.ferhatt.cryptoapplication.model.Crypto
+import com.ferhatt.cryptoapplication.repository.CryptoRepository
+import com.ferhatt.cryptoapplication.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CryptoDetailViewModel @Inject constructor(
+    private val repository: CryptoRepository
+) : ViewModel() {
+
+    suspend fun getCrypto(id : String) : Resource<Crypto>{
+        return repository.getCrypto(id)
+    }
+
 }
