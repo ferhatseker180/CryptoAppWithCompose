749dbc4bc75f006d5bae15f80a52957c27497abb
ferhatseker180 <ferhatseker180@gmail.com>
2023-09-04 00:06:47 +0300

    Project started, Main Activity and Navigation finished. App Module, Services, Models and Repository created. We created searching algorithm. View designed and we finished this app.

--- app/src/main/java/com/ferhatt/cryptoapplication/view/CyptoDetailScreen.kt
+++ app/src/main/java/com/ferhatt/cryptoapplication/view/CyptoDetailScreen.kt
@@ -1,11 +1,95 @@
 package com.ferhatt.cryptoapplication.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.ferhatt.cryptoapplication.model.Crypto
+import com.ferhatt.cryptoapplication.util.Resource
+import com.ferhatt.cryptoapplication.viewmodel.CryptoDetailViewModel
+import javax.annotation.meta.When
 
 @Composable
-fun CryptoDetailScreen(id : String,price : String,navController: NavController){
+fun CryptoDetailScreen(id : String, price : String,
+                       navController: NavController,
+                       viewModel : CryptoDetailViewModel = hiltViewModel()){
+
+
+    val cryptoItem = produceState<Resource<Crypto>>(initialValue = Resource.Loading()) {
+        value = viewModel.getCrypto(id)
+    }.value
+
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.secondary),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            when(cryptoItem) {
+
+                is Resource.Success -> {
+                    val selectedCrypto = cryptoItem.data!![0]
+                    Text(text = selectedCrypto.name,
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.inversePrimary,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Image(painter = rememberImagePainter(data = selectedCrypto.logo_url),
+                        contentDescription = selectedCrypto.name,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(200.dp, 200.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Gray, CircleShape)
+                    )
+
+                    Text(text = price,
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.inversePrimary,
+                        textAlign = TextAlign.Center
+
+                    )
+
+                }
+
+                is Resource.Error -> {
+                    Text(cryptoItem.message!!)
+                }
+
+                is Resource.Loading -> {
+                    CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+                }
+
+            }
+
+        }
+    }
 
 
 }
